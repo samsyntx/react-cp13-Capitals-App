59d8db5ed25170aeda668dc81fc7e9e2f2b19676
Shubham <samaroraytb@gmail.com>
2023-03-03 07:36:49 +0000

    React Options App

---
+++ src/components/Capitals/index.js
@@ -0,0 +1,62 @@
+import {Component} from 'react'
+import './index.css'
+
+const countryAndCapitalsList = [
+  {
+    id: 'NEW_DELHI',
+    capitalDisplayText: 'New Delhi',
+    country: 'India',
+  },
+  {
+    id: 'LONDON',
+    capitalDisplayText: 'London',
+    country: 'United Kingdom',
+  },
+  {
+    id: 'PARIS',
+    capitalDisplayText: 'Paris',
+    country: 'France',
+  },
+  {
+    id: 'KATHMANDU',
+    capitalDisplayText: 'Kathmandu',
+    country: 'Nepal',
+  },
+  {
+    id: 'HELSINKI',
+    capitalDisplayText: 'Helsinki',
+    country: 'Finland',
+  },
+]
+class Capitals extends Component {
+  changeInputEle = () => {}
+
+  render() {
+    const countryNameAsPerCapital = 'Hello'
+    return (
+      <div className="main-container">
+        <div className="content-container">
+          <h1 className="main-container-heading">Countries And Capitals</h1>
+          <div className="option-input-container">
+            <select
+              id="capitalDisplayText"
+              className="select-option-style"
+              onChange={this.changeInputEle}
+            >
+              {countryAndCapitalsList.map(eachCity => (
+                <option key={eachCity.id} value={eachCity.id}>
+                  {eachCity.capitalDisplayText}
+                </option>
+              ))}
+            </select>
+            <label htmlFor="capitalDisplayText">
+              is Capital of which country?
+            </label>
+          </div>
+          <h1 className="showing-country-style">{countryNameAsPerCapital}</h1>
+        </div>
+      </div>
+    )
+  }
+}
+export default Capitals
